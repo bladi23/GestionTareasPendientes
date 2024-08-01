4fc6741e1cf471b03da25187776a211d64c588ba
bladi23 <bladivela2003@hotmail.com>
2024-07-31 22:32:03 -0500

    Agregar archivos de proyecto.

---
+++ GestionTareasPendientes/Form1.Designer.cs
@@ -0,0 +1,223 @@
+﻿namespace GestionTareasPendientes
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Variable del diseñador necesaria.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Limpiar los recursos que se estén usando.
+        /// </summary>
+        /// <param name="disposing">true si los recursos administrados se deben desechar; false en caso contrario.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Código generado por el Diseñador de Windows Forms
+
+        /// <summary>
+        /// Método necesario para admitir el Diseñador. No se puede modificar
+        /// el contenido de este método con el editor de código.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.label1 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.button1 = new System.Windows.Forms.Button();
+            this.button2 = new System.Windows.Forms.Button();
+            this.bton_cancelar = new System.Windows.Forms.Button();
+            this.dateTimePicker2 = new System.Windows.Forms.DateTimePicker();
+            this.label3 = new System.Windows.Forms.Label();
+            this.label4 = new System.Windows.Forms.Label();
+            this.label5 = new System.Windows.Forms.Label();
+            this.label6 = new System.Windows.Forms.Label();
+            this.txt_tarea = new System.Windows.Forms.TextBox();
+            this.txt_materia = new System.Windows.Forms.TextBox();
+            this.txt_alumno = new System.Windows.Forms.TextBox();
+            this.list_tareas = new System.Windows.Forms.ListBox();
+            this.SuspendLayout();
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Font = new System.Drawing.Font("Microsoft Sans Serif", 20.25F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label1.Location = new System.Drawing.Point(289, 19);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(395, 31);
+            this.label1.TabIndex = 4;
+            this.label1.Text = "Gestor de Tareas Pendientes";
+            this.label1.Click += new System.EventHandler(this.label1_Click);
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(12, 515);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(135, 13);
+            this.label2.TabIndex = 5;
+            this.label2.Text = "ING. BLADIMIR VELASCO";
+            // 
+            // button1
+            // 
+            this.button1.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.button1.Location = new System.Drawing.Point(175, 415);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(81, 35);
+            this.button1.TabIndex = 18;
+            this.button1.Text = "Agregar";
+            this.button1.UseVisualStyleBackColor = true;
+            this.button1.Click += new System.EventHandler(this.button1_Click_1);
+            // 
+            // button2
+            // 
+            this.button2.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.button2.Location = new System.Drawing.Point(423, 415);
+            this.button2.Name = "button2";
+            this.button2.Size = new System.Drawing.Size(82, 35);
+            this.button2.TabIndex = 19;
+            this.button2.Text = "Eliminar";
+            this.button2.UseVisualStyleBackColor = true;
+            this.button2.Click += new System.EventHandler(this.button2_Click);
+            // 
+            // bton_cancelar
+            // 
+            this.bton_cancelar.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.bton_cancelar.Location = new System.Drawing.Point(295, 415);
+            this.bton_cancelar.Name = "bton_cancelar";
+            this.bton_cancelar.Size = new System.Drawing.Size(89, 35);
+            this.bton_cancelar.TabIndex = 20;
+            this.bton_cancelar.Text = "Cancelar";
+            this.bton_cancelar.UseVisualStyleBackColor = true;
+            // 
+            // dateTimePicker2
+            // 
+            this.dateTimePicker2.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.dateTimePicker2.Location = new System.Drawing.Point(385, 339);
+            this.dateTimePicker2.Name = "dateTimePicker2";
+            this.dateTimePicker2.Size = new System.Drawing.Size(200, 26);
+            this.dateTimePicker2.TabIndex = 21;
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label3.Location = new System.Drawing.Point(187, 157);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(55, 20);
+            this.label3.TabIndex = 22;
+            this.label3.Text = "Tarea";
+            this.label3.Click += new System.EventHandler(this.label3_Click_1);
+            // 
+            // label4
+            // 
+            this.label4.AutoSize = true;
+            this.label4.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label4.Location = new System.Drawing.Point(187, 220);
+            this.label4.Name = "label4";
+            this.label4.Size = new System.Drawing.Size(69, 20);
+            this.label4.TabIndex = 23;
+            this.label4.Text = "Materia";
+            // 
+            // label5
+            // 
+            this.label5.AutoSize = true;
+            this.label5.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label5.Location = new System.Drawing.Point(187, 278);
+            this.label5.Name = "label5";
+            this.label5.Size = new System.Drawing.Size(69, 20);
+            this.label5.TabIndex = 24;
+            this.label5.Text = "Alumno";
+            // 
+            // label6
+            // 
+            this.label6.AutoSize = true;
+            this.label6.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label6.Location = new System.Drawing.Point(187, 339);
+            this.label6.Name = "label6";
+            this.label6.Size = new System.Drawing.Size(131, 20);
+            this.label6.TabIndex = 25;
+            this.label6.Text = "Fecha de tarea";
+            // 
+            // txt_tarea
+            // 
+            this.txt_tarea.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.txt_tarea.Location = new System.Drawing.Point(398, 157);
+            this.txt_tarea.Name = "txt_tarea";
+            this.txt_tarea.Size = new System.Drawing.Size(187, 26);
+            this.txt_tarea.TabIndex = 27;
+            // 
+            // txt_materia
+            // 
+            this.txt_materia.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.txt_materia.Location = new System.Drawing.Point(398, 217);
+            this.txt_materia.Name = "txt_materia";
+            this.txt_materia.Size = new System.Drawing.Size(187, 26);
+            this.txt_materia.TabIndex = 28;
+            // 
+            // txt_alumno
+            // 
+            this.txt_alumno.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.txt_alumno.Location = new System.Drawing.Point(398, 272);
+            this.txt_alumno.Name = "txt_alumno";
+            this.txt_alumno.Size = new System.Drawing.Size(187, 26);
+            this.txt_alumno.TabIndex = 29;
+            // 
+            // list_tareas
+            // 
+            this.list_tareas.FormattingEnabled = true;
+            this.list_tareas.Location = new System.Drawing.Point(616, 148);
+            this.list_tareas.Name = "list_tareas";
+            this.list_tareas.Size = new System.Drawing.Size(347, 225);
+            this.list_tareas.TabIndex = 30;
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(975, 537);
+            this.Controls.Add(this.list_tareas);
+            this.Controls.Add(this.txt_alumno);
+            this.Controls.Add(this.txt_materia);
+            this.Controls.Add(this.txt_tarea);
+            this.Controls.Add(this.label6);
+            this.Controls.Add(this.label5);
+            this.Controls.Add(this.label4);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.dateTimePicker2);
+            this.Controls.Add(this.bton_cancelar);
+            this.Controls.Add(this.button2);
+            this.Controls.Add(this.button1);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.label1);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.Button button2;
+        private System.Windows.Forms.Button bton_cancelar;
+        private System.Windows.Forms.DateTimePicker dateTimePicker2;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.Label label4;
+        private System.Windows.Forms.Label label5;
+        private System.Windows.Forms.Label label6;
+        private System.Windows.Forms.TextBox txt_tarea;
+        private System.Windows.Forms.TextBox txt_materia;
+        private System.Windows.Forms.TextBox txt_alumno;
+        private System.Windows.Forms.ListBox list_tareas;
+    }
+}
+
---
+++ GestionTareasPendientes/Form1.cs
@@ -0,0 +1,86 @@
+﻿
+
+namespace GestionTareasPendientes
+{
+    using System;
+    using System.Collections.Generic;
+    using System.ComponentModel;
+    using System.Data;
+    using System.Drawing;
+    using System.Linq;
+    using System.Text;
+    using System.Threading.Tasks;
+    using System.Windows.Forms;
+
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        
+        private void listBox1_SelectedIndexChanged(object sender, EventArgs e)
+        {
+
+        }
+
+        
+
+        private void textBox1_TextChanged(object sender, EventArgs e)
+        {
+                                                                    
+        }
+
+        private void label1_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void label3_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void label3_Click_1(object sender, EventArgs e)
+        {
+
+        }
+
+        private void button1_Click_1(object sender, EventArgs e)
+        {
+            var tarea = txt_tarea.Text;
+            var materia = txt_materia.Text;
+            var alumno = txt_alumno.Text;
+            var fecha = dateTimePicker2.Text;
+
+            string cadena = tarea + " // " + materia + "  // " + alumno + " // " + fecha;
+            list_tareas.Items.Add(cadena);
+
+
+        }
+        public void limpiarCaja()
+        {
+            txt_tarea.Text = "";
+            txt_materia.Text = "";
+            txt_alumno.Text = "";
+            
+            
+
+        }
+
+        private void button2_Click(object sender, EventArgs e)
+        {
+            if ( list_tareas.SelectedIndex != -1 )
+            {
+                DialogResult dialogResult = MessageBox.Show("¿Está seguro de eliminar la tarea?", "Eliminar tarea", MessageBoxButtons.YesNo);
+                list_tareas.Items.RemoveAt(list_tareas.SelectedIndex);
+            }
+            else
+            {
+                MessageBox.Show("Seleccione una tarea");
+            }
+        }
+            
+    }
+}
